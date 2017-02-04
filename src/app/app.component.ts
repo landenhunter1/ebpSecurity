70997e1f7b76bcef5af1267914db83f6406b2134
Hunter <Landen.Hunter@us.sogeti.com>
2017-02-04 16:30:09 -0500

    Initial Commit, HTML implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; 
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  //template: '<h1>Hello {{name}}</h1>', 
+  templateUrl: './app/app.component.html' 
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { name = 'EBP Security Check'; }
